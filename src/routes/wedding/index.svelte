92f892536ab29d64b7b80e188956c8dd4e7466ee
Spencer Stream <sprewstream@gmail.com>
2020-10-04 11:18:17 -0500

    Starting new 'wedding' subdirectory

---
+++ src/routes/wedding/index.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<title>Katherine and Spencer's Wedding</title>
+</svelte:head>
+
+<h1>Great success!</h1>
+
+<figure>
+	<img alt='Success Kid' src='successkid.jpg'>
+	<figcaption>Have fun with Sapper!</figcaption>
+</figure>
+
+<p><strong>Welcome!</strong></p>
