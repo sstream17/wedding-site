d119f6a889df4ee97dd0a74969bc2abe32517ece
Spencer Stream <sprewstream@gmail.com>
2020-10-04 11:53:56 -0500

    Updating style of Nav component to use new color and to center items

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -4,9 +4,10 @@
 
 <style>
 	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
+		border-bottom: 1px solid rgba(255, 192, 203, 0.1);
 		font-weight: 300;
 		padding: 0 1em;
+		text-align: center;
 	}
 
 	ul {
@@ -16,14 +17,13 @@
 
 	/* clearfix */
 	ul::after {
-		content: '';
+		content: "";
 		display: block;
 		clear: both;
 	}
 
 	li {
-		display: block;
-		float: left;
+		display: inline-block;
 	}
 
 	[aria-current] {
@@ -33,10 +33,10 @@
 
 	[aria-current]::after {
 		position: absolute;
-		content: '';
+		content: "";
 		width: calc(100% - 1em);
 		height: 2px;
-		background-color: rgb(255,62,0);
+		background-color: rgb(255, 192, 203);
 		display: block;
 		bottom: -1px;
 	}
@@ -50,12 +50,40 @@
 
 <nav>
 	<ul>
-		<li><a aria-current="{segment === 'schedule' ? 'page' : undefined}" href="/wedding/schedule">Schedule</a></li>
-		<li><a aria-current="{segment === 'travel' ? 'page' : undefined}" href="/wedding/travel">Travel</a></li>
-		<li><a aria-current="{segment === 'photos' ? 'page' : undefined}" href="/wedding/photos">Photos</a></li>
-		<li><a aria-current="{segment === 'wedding_party' ? 'page' : undefined}" href="/wedding/wedding_party">Wedding Party</a></li>
-		<li><a aria-current="{segment === 'registry' ? 'page' : undefined}" href="/wedding/registry">Registry</a></li>
-		<li><a aria-current="{segment === 'faq' ? 'page' : undefined}" href="/wedding/faq">FAQ</a></li>
-		<li><a aria-current="{segment === 'rsvp' ? 'page' : undefined}" href="/wedding/rsvp">RSVP</a></li>
+		<li>
+			<a
+				aria-current={segment === 'schedule' ? 'page' : undefined}
+				href="/wedding/schedule">Schedule</a>
+		</li>
+		<li>
+			<a
+				aria-current={segment === 'travel' ? 'page' : undefined}
+				href="/wedding/travel">Travel</a>
+		</li>
+		<li>
+			<a
+				aria-current={segment === 'photos' ? 'page' : undefined}
+				href="/wedding/photos">Photos</a>
+		</li>
+		<li>
+			<a
+				aria-current={segment === 'wedding_party' ? 'page' : undefined}
+				href="/wedding/wedding_party">Wedding Party</a>
+		</li>
+		<li>
+			<a
+				aria-current={segment === 'registry' ? 'page' : undefined}
+				href="/wedding/registry">Registry</a>
+		</li>
+		<li>
+			<a
+				aria-current={segment === 'faq' ? 'page' : undefined}
+				href="/wedding/faq">FAQ</a>
+		</li>
+		<li>
+			<a
+				aria-current={segment === 'rsvp' ? 'page' : undefined}
+				href="/wedding/rsvp">RSVP</a>
+		</li>
 	</ul>
 </nav>
