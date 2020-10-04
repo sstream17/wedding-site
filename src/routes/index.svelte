32d67c9292a41a597ac62f273afc702c7561cb54
Spencer Stream <sprewstream@gmail.com>
2020-10-04 10:33:27 -0500

    Updating the welcome message

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -43,4 +43,4 @@
 	<figcaption>Have fun with Sapper!</figcaption>
 </figure>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<p><strong>Welcome!</strong></p>
