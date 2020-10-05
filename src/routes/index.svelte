d4df1a160e45bcbd60fdc4f6685dccd5c19d0ed5
Spencer Stream <sprewstream@gmail.com>
2020-10-05 16:52:01 -0500

    Setting up domain root to redirect to /wedding

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,49 +1,9 @@
-<style>
-	* {
-		text-align: center;
+<script context="module">
+	export async function preload(page, session) {
+		return this.redirect(301, "/wedding");
 	}
-
-	h1 {
-		margin: 0 auto;
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	.button-container {
-		display: flex;
-		justify-content: center;
-	}
-
-	.button {
-		text-decoration: none;
-		display: inline-block;
-		padding: 1rem;
-		border-radius: 0.25rem;
-		background: rgb(255, 192, 203);
-		color: #202020;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
+</script>
 
 <svelte:head>
-	<title>Katherine & Spencer</title>
+	<title>Katherine & Spencer's Wedding</title>
 </svelte:head>
-
-<h1>We're getting married!</h1>
-
-<h2>
-	Visit our site for to see details about the wedding, browse the registry,
-	and RSVP
-</h2>
-
-<div class="button-container">
-	<a href="/wedding" class="button" role="button">Katherine & Spencer's
-		Wedding</a>
-</div>
