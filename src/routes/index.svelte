32d5ad3233ca2efd676a18e6ee6795f465a9727c
Spencer Stream <sprewstream@gmail.com>
2020-10-05 17:24:49 -0500

    Removing redirect in case it was the cause of the PROD error

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,5 @@
-<script context="module">
-	export async function preload(page, session) {
-		return this.redirect(301, "/wedding");
-	}
-</script>
-
 <svelte:head>
 	<title>Katherine & Spencer's Wedding</title>
 </svelte:head>
+
+<a href="/wedding">Site</a>
