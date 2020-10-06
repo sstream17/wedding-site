02091bcbdb754d3d9a45c90ac89f119502b8067e
Spencer Stream <sprewstream@gmail.com>
2020-10-06 10:41:18 -0500

    Revert "Updating base path to /wedding-site"
    This reverts commit 37189aeaa9e9814acd90512227099271cc79afdc.

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,6 @@ const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
 	.use(
-		'/wedding-site',
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
