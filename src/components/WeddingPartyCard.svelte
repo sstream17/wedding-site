710f234ec7761166f0044316fbe63e47d847f2cd
Spencer Stream <sprewstream@gmail.com>
2021-01-23 11:35:11 -0600

    Adding wedding party info

---
+++ src/components/WeddingPartyCard.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let name: string;
+    export let role: string;
+</script>
+
+<style>
+    .wedding-party-card {
+        margin-bottom: 4rem;
+        text-align: center;
+    }
+</style>
+
+<div class="wedding-party-card">
+    <h2>{name}</h2>
+    <h3>{role}</h3>
+    <slot></slot>
+</div>
