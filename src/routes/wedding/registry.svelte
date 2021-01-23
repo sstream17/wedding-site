5ae87dcd1a525515918fbffc38f1b1b3cd10c606
Spencer Stream <sprewstream@gmail.com>
2021-01-23 12:04:20 -0600

    Adding Wayfair registry

--- src/routes/wedding/registry.svelte
+++ src/routes/wedding/registry.svelte
@@ -1,9 +1,3 @@
-<style>
-    .full-width {
-        width: 100%;
-    }
-</style>
-
 <h1>Registry</h1>
 
 <a href="https://www.bedbathandbeyond.com/store/giftregistry/viewregistryguest/549824664?selectedRLVFilters=_recommended">
@@ -12,4 +6,8 @@
 <br />
 <a href="https://www.target.com/gift-registry/giftgiver?registryId=ef3b88706ae34d38a65c6cc71536b129&type=WEDDING">
     Target
+</a>
+<br />
+<a href="https://www.wayfair.com/registry/wedding/990aa1d2-7742-43a0-b5d4-8541194d749c">
+    Wayfair
 </a>
