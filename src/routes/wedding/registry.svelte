299ca73fd15064e32f470886801dbaab09642192
Spencer Stream <sprewstream@gmail.com>
2021-01-23 11:57:54 -0600

    Adding links to registries

--- src/routes/wedding/registry.svelte
+++ src/routes/wedding/registry.svelte
@@ -1,1 +1,15 @@
+<style>
+    .full-width {
+        width: 100%;
+    }
+</style>
+
 <h1>Registry</h1>
+
+<a href="https://www.bedbathandbeyond.com/store/giftregistry/viewregistryguest/549824664?selectedRLVFilters=_recommended">
+    Bed Bath & Beyond
+</a>
+<br />
+<a href="https://www.target.com/gift-registry/giftgiver?registryId=ef3b88706ae34d38a65c6cc71536b129&type=WEDDING">
+    Target
+</a>
