4c9e7e01005cb876abef4089544f04d3e0c7711d
Spencer Stream <sprewstream@gmail.com>
2021-05-29 11:18:36 -0500

    Removing periods from wedding party bios

--- src/routes/wedding/wedding_party.svelte
+++ src/routes/wedding/wedding_party.svelte
@@ -8,54 +8,54 @@
         <WeddingPartyCard
             name="Elissa Fuelberth"
             role="Maid of Honor">
-            <p>The bride's best friend.</p>
+            <p>The bride's best friend</p>
         </WeddingPartyCard>
         <WeddingPartyCard
             name="Emily Wiles"
             role="Bridesmaid">
-            <p>Cousin of the bride.</p>
+            <p>Cousin of the bride</p>
         </WeddingPartyCard>
         <WeddingPartyCard
             name="Sammy Wiles"
             role="Bridesmaid">
-            <p>Cousin of the bride.</p>
+            <p>Cousin of the bride</p>
         </WeddingPartyCard>
         <WeddingPartyCard
             name="Molli Curry"
             role="Bridesmaid">
-            <p>Friend of the bride.</p>
+            <p>Friend of the bride</p>
         </WeddingPartyCard>
         <WeddingPartyCard
             name="Lauren Nunez"
             role="Bridesmaid">
-            <p>Friend of the bride.</p>
+            <p>Friend of the bride</p>
         </WeddingPartyCard>
     </div>
     <div class="flex-col">
         <WeddingPartyCard
             name="Matthew Schaaf"
             role="Best Man">
-            <p>The groom's best friend.</p>
+            <p>The groom's best friend</p>
         </WeddingPartyCard>
         <WeddingPartyCard
             name="Zak Keck"
             role="Groomsman">
-            <p>Friend of the groom.</p>
+            <p>Friend of the groom</p>
         </WeddingPartyCard>
         <WeddingPartyCard
             name="Syrus Bomberger"
             role="Groomsman">
-            <p>Friend of the groom.</p>
+            <p>Friend of the groom</p>
         </WeddingPartyCard>
         <WeddingPartyCard
             name="Chase Grossman"
             role="Groomsman">
-            <p>Cousin of the groom.</p>
+            <p>Cousin of the groom</p>
         </WeddingPartyCard>
         <WeddingPartyCard
             name="Drew Stream"
             role="Groomsman">
-            <p>Brother of the groom.</p>
+            <p>Brother of the groom</p>
         </WeddingPartyCard>
     </div>
 </div>
