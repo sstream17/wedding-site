de8c4b56af71a2cfef9f6319e6ef456aa4e35da1
Spencer Stream <sprewstream@gmail.com>
2021-07-22 20:55:29 -0500

    Binding values to the inputs to allow form submission. Using a CSS grid to align the form items

--- src/routes/wedding/rsvp.svelte
+++ src/routes/wedding/rsvp.svelte
@@ -1,1 +1,62 @@
+<script lang="ts">
+    let name: string;
+    let numberGuests: number = 1;
+    let notes: string;
+
+    const handleSubmit = () => {
+        console.log(`submitting ${name} for ${numberGuests}`);
+    };
+</script>
+
 <h1>RSVP</h1>
+
+<form on:submit|preventDefault={handleSubmit}>
+    <div class="grid">
+        <label for="name">Your name</label>
+        <input id="name" type="text" required bind:value={name} />
+
+        <label for="guests">How many guests including yourself?</label>
+        <input id="guests" type="number" required bind:value={numberGuests} />
+
+        <label for="notes">Any additional notes</label>
+        <textarea id="notes" required bind:value={notes} />
+
+        <div />
+        <input type="submit" />
+    </div>
+</form>
+
+<style>
+    label {
+        text-align: end;
+    }
+
+    textarea {
+        resize: vertical;
+    }
+
+    .grid {
+        display: grid;
+        grid-template-columns: 3fr 2fr;
+        gap: 1em 1em;
+        justify-content: center;
+        align-content: center;
+        margin: 0 auto;
+        width: max-content;
+    }
+
+    @media screen and (max-width: 768px) {
+        label {
+            text-align: start;
+        }
+
+        input, textarea {
+            margin-bottom: 1em;
+        }
+
+        .grid {
+            grid-template-columns: 1fr;
+            gap: 0;
+        }
+    }
+</style>
