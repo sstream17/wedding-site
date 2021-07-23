85d4f84ae5731130096982576f70ac6e6387aebb
Spencer Stream <sprewstream@gmail.com>
2021-07-22 22:51:46 -0500

    Starting to set up mailto link and using localStorage to track the status of the RSVP

--- src/routes/wedding/rsvp.svelte
+++ src/routes/wedding/rsvp.svelte
@@ -1,49 +1,71 @@
 <script lang="ts">
+    import { onMount } from "svelte";
+
     let name: string;
     let numberAdults: number = 1;
     let numberChildren: number = 0;
     let notes: string;
+    let submitted: boolean = false;
 
     const handleSubmit = () => {
-        console.log(`submitting ${name} for ${numberAdults}`);
+        localStorage.setItem("rsvp", "true");
+        const message =
+            "Thank you for inviting us to join your wedding celebration.";
+        window.open(
+            `mailto:rsvp@katherineandspencer.com?subject=${encodeURI(
+                name
+            )}%20RSVP&body=${encodeURI(message)}`
+        );
+        submitted = true;
     };
+
+    onMount(() => {
+        submitted = !!localStorage.getItem("rsvp");
+    });
 </script>
 
 <h1>RSVP</h1>
 
-<form on:submit|preventDefault={handleSubmit}>
-    <div class="grid">
-        <label for="name">Your name*</label>
-        <input id="name" type="text" required bind:value={name} />
+{#if !submitted}
+    <form on:submit|preventDefault={handleSubmit}>
+        <div class="grid">
+            <label for="name">Your name*</label>
+            <input id="name" type="text" required bind:value={name} />
 
-        <label for="adults">How many adults including yourself?*</label>
-        <input
-            id="adults"
-            type="number"
-            min="1"
-            required
-            bind:value={numberAdults}
-        />
+            <label for="adults">How many adults including yourself?*</label>
+            <input
+                id="adults"
+                type="number"
+                min="1"
+                required
+                bind:value={numberAdults}
+            />
 
-        <label for="children">How many children?*</label>
-        <input
-            id="children"
-            type="number"
-            min="0"
-            required
-            bind:value={numberChildren}
-        />
+            <label for="children">How many children?*</label>
+            <input
+                id="children"
+                type="number"
+                min="0"
+                required
+                bind:value={numberChildren}
+            />
 
-        <label for="notes">Any additional notes</label>
-        <textarea id="notes" bind:value={notes} />
+            <label for="notes">Any additional notes</label>
+            <textarea id="notes" bind:value={notes} />
 
-        <div />
-        <input type="submit" value="Send" />
+            <div />
+            <input type="submit" value="Send" />
 
-        <div />
-        <div>* Required</div>
-    </div>
-</form>
+            <div />
+            <div>* Required</div>
+        </div>
+    </form>
+{:else}
+    <h2>
+        Thank you for your RSVP. We look forward to you joining us on our
+        special day!
+    </h2>
+{/if}
 
 <style>
     label {
