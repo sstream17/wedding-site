bf43a7312c2df09489c2b91aebb322677d568dd2
Spencer Stream <sprewstream@gmail.com>
2021-07-27 17:52:54 -0500

    Using CSS grid to fix the layout of the FAQCard items

--- src/components/FAQCard.svelte
+++ src/components/FAQCard.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
     export let q: string;
     export let a: string;
+    export let hideHorizontalDivider: boolean = false;
 </script>
 
-<div class="flex-row flex-wrap justify-content-center align-items-baseline">
-    <h2>{q}</h2>
-    <div class="divider" />
-    <p>{a}</p>
+<div class="grid justify-content-center">
+    <h2 class="question">{q}</h2>
+    <div class="divider-v" />
+    <p class="answer">{a}</p>
+    {#if !hideHorizontalDivider}
+        <div class="divider-h" />
+    {/if}
 </div>
 
 <style>
@@ -14,9 +18,53 @@
         text-align: center;
     }
 
-    .divider {
+    .grid {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        place-items: center;
+        margin-bottom: 4rem;
+    }
+
+    .question {
+        justify-self: end;
+    }
+
+    .divider-v {
         border-left: 1px solid white;
-        height: 200px;
-        transform: translate(0, 50%);
+        height: 100px;
+    }
+
+    .answer {
+        justify-self: start;
+    }
+
+    .divider-h {
+        display: none;
+        border-bottom: 1px solid white;
+        width: 200px;
+    }
+
+    @media screen and (max-width: 768px) {
+        .grid {
+            display: grid;
+            grid-template-columns: 1fr;
+            place-items: center;
+        }
+
+        .question {
+            justify-self: center;
+        }
+
+        .divider-v {
+            display: none;
+        }
+
+        .answer {
+            justify-self: center;
+        }
+
+        .divider-h {
+            display: block;
+        }
     }
 </style>
--- src/routes/wedding/faq.svelte
+++ src/routes/wedding/faq.svelte
@@ -4,4 +4,8 @@
 
 <h1>FAQ</h1>
 
-<FaqCard q="Can I bring a guest?" a="No sorry." />
+<FaqCard
+    q="Can I bring a guest?"
+    a="If your invitation mentions a guest, you may. Otherwise we would prefer if it was just you."
+/>
+<FaqCard q="Can I bring my kids?" a="Seriously?" hideHorizontalDivider="true" />
