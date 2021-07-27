e99132c547d99bc7ccb8954813e9c47436416401
Spencer Stream <sprewstream@gmail.com>
2021-07-26 21:28:40 -0500

    Starting to setup FAQ card. Still needs styling and additional work

---
+++ src/components/FAQCard.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    export let q: string;
+    export let a: string;
+</script>
+
+<div class="flex-row flex-wrap justify-content-center align-items-baseline">
+    <h2>{q}</h2>
+    <div class="divider" />
+    <p>{a}</p>
+</div>
+
+<style>
+    p {
+        text-align: center;
+    }
+
+    .divider {
+        border-left: 1px solid white;
+        height: 200px;
+        transform: translate(0, 50%);
+    }
+</style>
--- src/routes/wedding/faq.svelte
+++ src/routes/wedding/faq.svelte
@@ -1,1 +1,7 @@
+<script>
+    import FaqCard from "../../components/FAQCard.svelte";
+</script>
+
 <h1>FAQ</h1>
+
+<FaqCard q="Can I bring a guest?" a="No sorry." />
