039b615f08a45d98dee095fd6a01702804f269c5
Spencer Stream <sprewstream@gmail.com>
2021-07-26 20:52:56 -0500

    Adding bios for the groomsmen

--- src/routes/wedding/wedding_party.svelte
+++ src/routes/wedding/wedding_party.svelte
@@ -103,13 +103,10 @@
             img="./wedding_party/matthew.jpg"
         >
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Matthew and Spencer have been best friends for over 12 years
+                after meeting in 6th grade. They shared many classes but had the
+                most fun in band. Matthew’s best memories with Spencer come from
+                the time they spent during middle school band camp at UNL.
             </p>
         </WeddingPartyCard>
         <WeddingPartyCard
@@ -118,13 +115,12 @@
             img="./wedding_party/zak.jpg"
         >
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Zak and Spencer met as freshman in college in the UNL Marching
+                Band. After getting stuck sitting next to Spencer, what Zak
+                thought would be the most annoying 13 hour bus ride of his life
+                ended up giving him one of his closest friends. Spencer and Zak
+                roomed together for a few years in college, and Zak takes credit
+                for introducing Spencer to computer programming.
             </p>
         </WeddingPartyCard>
         <WeddingPartyCard
@@ -133,13 +129,11 @@
             img="./wedding_party/syrus.jpg"
         >
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Syrus and Spencer went to high school together and always had a
+                similar friend group. They became closer friends after sharing
+                computer science classes in college. Syrus and Spencer competed
+                together multiple times at the UNL Hackathon and won prizes for
+                their acclaimed programs RecycloBot and coder_royale.
             </p>
         </WeddingPartyCard>
         <WeddingPartyCard
@@ -148,13 +142,13 @@
             img="./wedding_party/nicholas.jpg"
         >
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Nick and Spencer became good friends in high school marching
+                band. Spencer would often make goofy jokes while waiting for
+                instructions from Mr. Metteer. Nick could never hold back
+                laughter long enough to avoid attention, and would get called
+                out in front of the entire band. They became known for this and
+                it was increasingly challenging to avoid the constant
+                embarrassment from Mr. Metteer.
             </p>
         </WeddingPartyCard>
         <WeddingPartyCard
@@ -163,13 +157,11 @@
             img="./wedding_party/chase.jpg"
         >
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                As Spencer’s older cousin, Chase has known Spencer his entire
+                life. They would spend countless days in the summer playing
+                football, basketball, and of course Xbox. Chase and Spencer have
+                always been extremely close, you may have even mistaken them to
+                be brothers.
             </p>
         </WeddingPartyCard>
         <WeddingPartyCard
@@ -178,13 +170,12 @@
             img="./wedding_party/drew.jpg"
         >
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Drew is Spencer’s younger brother, and has always looked up to
+                Spencer as his greatest role model. They always had a lot of fun
+                together on family vacations to Colorado, Tennessee, and
+                amusement parks around the country. Drew may be the cooler of
+                the two brothers, but don’t let their differences distract you
+                from their love for each other.
             </p>
         </WeddingPartyCard>
     </div>
