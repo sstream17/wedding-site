27ce64994f84d72485a752ae85a7fed1ca5b6b13
Spencer Stream <sprewstream@gmail.com>
2021-07-29 22:33:34 -0500

    Starting to add flowers to the background of the site's layout

--- src/routes/wedding/_layout.svelte
+++ src/routes/wedding/_layout.svelte
@@ -10,7 +10,9 @@
 	class:center-content={segment !== undefined}
 	class:wider={segment === 'wedding_party'}
 >
+	<div class="flowers top" />
 	<slot />
+	<div class="flowers bottom" />
 </main>
 
 <style>
@@ -27,5 +29,27 @@
 		position: relative;
 		margin: 0 auto;
 		box-sizing: border-box;
+	}
+
+	.flowers {
+		position: absolute;
+		height: 200px;
+		width: 400px;
+		z-index: -10;
+		background-size: contain;
+		background-repeat: no-repeat;
+	}
+
+	.top {
+		top: 0;
+		right: 0;
+		background-image: url("/flowers_top.png");
+		background-position-x: 100%;
+	}
+
+	.bottom {
+		left: 0;
+		bottom: 0;
+		background-image: url("/flowers_bottom.png");
 	}
 </style>
