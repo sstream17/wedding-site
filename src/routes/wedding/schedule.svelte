28a6fff010dbc478947d01556bcfa6a9708a5348
Spencer Stream <sprewstream@gmail.com>
2021-08-01 18:59:26 -0500

    Adding icons to the schedule page and using the DividedCard component to better style the items

--- src/routes/wedding/schedule.svelte
+++ src/routes/wedding/schedule.svelte
@@ -1,4 +1,5 @@
 <script>
+    import DividedCard from "../../components/DividedCard.svelte";
     import Seo from "../../components/SEO.svelte";
 </script>
 
@@ -8,27 +9,65 @@
 />
 
 <h1>Schedule</h1>
-
-<h2>Ceremony</h2>
-<h3>Saturday, June 18, 2022</h3>
-<h4>4:00 pm</h4>
 <p>
-    The ceremony will be held in Ballroom B-E. You may arrive as early as
-    3:30pm. There will be plenty of parking outside the Holthus.
+    Join us at the Holthus Convention Center in York. Below you will find what
+    to expect for our wedding night!
 </p>
 
-<h2>Cocktail Hour</h2>
-<h3>Saturday, June 18, 2022</h3>
-<h4>5:00 pm</h4>
-<p>
-    A cocktail hour will be held in the Holthus lobby between the ceremony and
-    the reception. Feel free to grab a drink and mingle with other guests!
-</p>
+<DividedCard>
+    <div slot="left">
+        <h2>Ceremony</h2>
+        <h3>Saturday, June 18, 2022</h3>
+        <h4>4:00 pm</h4>
+    </div>
+    <div slot="right">
+        <img src="./schedule/Rings.svg" alt="Ceremony" />
+        <p>
+            The ceremony will be held in Ballroom B-E. You may arrive as early
+            as 3:30pm. There will be plenty of parking outside the Holthus.
+        </p>
+    </div>
+</DividedCard>
 
-<h2>Reception</h2>
-<h3>Saturday, June 18, 2022</h3>
-<h4>6:00 pm</h4>
-<p>
-    The reception will start at 6pm in Ballroom A. Follow the signs in the lobby
-    to find your way there for dinner, dancing, and cake!
-</p>
+<DividedCard>
+    <div slot="left">
+        <h2>Cocktail Hour</h2>
+        <h3>Saturday, June 18, 2022</h3>
+        <h4>5:00 pm</h4>
+    </div>
+    <div slot="right">
+        <img src="./schedule/Cheers.svg" alt="Cocktail Hour" />
+        <p>
+            A cocktail hour will be held in the Holthus lobby between the
+            ceremony and the reception. Feel free to grab a drink and mingle
+            with other guests!
+        </p>
+    </div>
+</DividedCard>
+
+<DividedCard hideHorizontalDivider="true">
+    <div slot="left">
+        <h2>Reception</h2>
+        <h3>Saturday, June 18, 2022</h3>
+        <h4>6:00 pm</h4>
+    </div>
+    <div slot="right">
+        <img src="./schedule/Cake.svg" alt="Reception" />
+        <p>
+            The reception will start at 6pm in Ballroom A. Follow the signs in
+            the lobby to find your way there for dinner, dancing, and cake!
+        </p>
+    </div>
+</DividedCard>
+
+<style>
+    img {
+        display: block;
+        height: 50px;
+        margin: 0 auto;
+    }
+
+    p {
+        text-align: center;
+    }
+</style>
