18236c6e49317495ef0ce6848b6baccfecb98a33
Spencer Stream <sprewstream@gmail.com>
2021-08-01 17:53:14 -0500

    Adding details to the schedule

--- src/routes/wedding/schedule.svelte
+++ src/routes/wedding/schedule.svelte
@@ -8,3 +8,27 @@
 />
 
 <h1>Schedule</h1>
+
+<h2>Ceremony</h2>
+<h3>Saturday, June 18, 2022</h3>
+<h4>4:00 pm</h4>
+<p>
+    The ceremony will be held in Ballroom B-E. You may arrive as early as
+    3:30pm. There will be plenty of parking outside the Holthus.
+</p>
+
+<h2>Cocktail Hour</h2>
+<h3>Saturday, June 18, 2022</h3>
+<h4>5:00 pm</h4>
+<p>
+    A cocktail hour will be held in the Holthus lobby between the ceremony and
+    the reception. Feel free to grab a drink and mingle with other guests!
+</p>
+
+<h2>Reception</h2>
+<h3>Saturday, June 18, 2022</h3>
+<h4>6:00 pm</h4>
+<p>
+    The reception will start at 6pm in Ballroom A. Follow the signs in the lobby
+    to find your way there for dinner, dancing, and cake!
+</p>
