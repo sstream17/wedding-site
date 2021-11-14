6cddb0e173e16f3e71ba18c19003f5860405e201
Spencer Stream <sprewstream@gmail.com>
2021-11-14 15:35:49 -0600

    Allowing users to bypass the deadline warning

--- src/components/rsvp/ClosedRSVP.svelte
+++ src/components/rsvp/ClosedRSVP.svelte
@@ -1,1 +1,10 @@
+<script lang="ts">
+    export let continueToForm: boolean = false;
+
+    const onClick = () => {
+        continueToForm = true;
+    };
+</script>
+
 Sorry
+<input type="submit" value="I understand" on:click={onClick} />
--- src/routes/wedding/rsvp.svelte
+++ src/routes/wedding/rsvp.svelte
@@ -3,16 +3,18 @@
     import Rsvp from "../../components/rsvp/RSVP.svelte";
     import Seo from "../../components/SEO.svelte";
 
-    const deadline = new Date(2022, 5, 28);
+    const deadline = new Date(2021, 5, 28);
     const today = new Date();
+
+    let continueToForm: boolean = false;
 </script>
 
 <Seo title="RSVP" description="Send your RSVP right from our website." />
 
 <h1>RSVP</h1>
 
-{#if today > deadline}
-    <ClosedRsvp />
+{#if !continueToForm && today > deadline}
+    <ClosedRsvp bind:continueToForm />
 {:else}
     <Rsvp />
 {/if}
