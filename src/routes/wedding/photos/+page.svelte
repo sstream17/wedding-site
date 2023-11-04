5e8b9a563d54a1e7036f643da3c02979f4d8bc5d
Spencer Stream <sprewstream@gmail.com>
2023-11-04 16:43:41 -0500

    Updating photos page to layout pictures dynamically, rather than relying on CSS grid and media query width

--- src/routes/wedding/photos/+page.svelte
+++ src/routes/wedding/photos/+page.svelte
@@ -8,15 +8,17 @@
 
 <h2>Wedding</h2>
 
-<div class="grid">
-    <div class="zoom img-4">
+<div class="flex-row flex-wrap photo-grid">
+    <div class="zoom">
         <img src="/photos/katie+spencer4.jpg" alt="Katie and Spencer" />
     </div>
-    <div class="zoom img-5">
-        <img src="/photos/wedding_party1.jpg" alt="The wedding party" />
-    </div>
-    <div class="zoom img-6">
-        <img src="/photos/katie+spencer5.jpg" alt="Katie and Spencer" />
+    <div class="flex-col photo-stack">
+        <div class="zoom">
+            <img src="/photos/wedding_party1.jpg" alt="The wedding party" />
+        </div>
+        <div class="zoom">
+            <img src="/photos/katie+spencer5.jpg" alt="Katie and Spencer" />
+        </div>
     </div>
 </div>
 
@@ -29,14 +31,16 @@
 
 <h2>Engagement</h2>
 
-<div class="grid">
-    <div class="zoom img-1">
-        <img src="/photos/katie+spencer1.jpg" alt="Katie and Spencer" />
+<div class="flex-row flex-wrap photo-grid">
+    <div class="flex-col photo-stack">
+        <div class="zoom">
+            <img src="/photos/katie+spencer1.jpg" alt="Katie and Spencer" />
+        </div>
+        <div class="zoom">
+            <img src="/photos/katie+spencer2.jpg" alt="Katie and Spencer" />
+        </div>
     </div>
-    <div class="zoom img-2">
-        <img src="/photos/katie+spencer2.jpg" alt="Katie and Spencer" />
-    </div>
-    <div class="zoom img-3">
+    <div class="zoom">
         <img src="/photos/katie+spencer3.jpg" alt="Katie and Spencer" />
     </div>
 </div>
@@ -49,82 +53,44 @@
 </p>
 
 <style>
+    * {
+        --image-size: 400px;
+    }
+
+    .photo-grid {
+        gap: 1em;
+        min-height: 70vh;
+    }
+
+    .photo-grid > * {
+        flex-basis: var(--image-size);
+        min-height: var(--image-size);
+        flex-grow: 1;
+        flex-shrink: 1;
+    }
+
+    .photo-stack {
+        gap: 1em;
+    }
+
+    .photo-stack > * {
+        flex-basis: var(--image-size);
+    }
+
     .zoom {
         position: relative;
         overflow: hidden;
-        padding: 50%;
     }
 
     img {
-        position: absolute;
-        top: 0;
-        left: 0;
         width: 100%;
         height: 100%;
-        will-change: transform;
         object-fit: cover;
+        will-change: transform;
         transition: transform 1s;
     }
 
     img:hover {
         transform: scale(1.1);
-    }
-
-    .grid {
-        display: grid;
-        grid-template-columns: repeat(8, 1fr);
-        grid-template-rows: repeat(6, 1fr);
-        gap: 1em;
-    }
-
-    .img-1 {
-        grid-column: 1 / span 4;
-        grid-row: 1 / span 3;
-    }
-
-    .img-2 {
-        grid-column: 5 / span 4;
-        grid-row: 1 / span 6;
-    }
-
-    .img-3 {
-        grid-column: 1 / span 4;
-        grid-row: 4 / span 3;
-    }
-
-    .img-4 {
-        grid-column: 1 / span 4;
-        grid-row: 1 / span 6;
-    }
-
-    .img-5 {
-        grid-column: 5 / span 4;
-        grid-row: 1 / span 3;
-    }
-
-    .img-6 {
-        grid-column: 5 / span 4;
-        grid-row: 4 / span 3;
-    }
-
-    @media screen and (max-width: 768px) {
-        .grid {
-            grid-template-columns: 1fr;
-        }
-
-        .img-1 {
-            grid-column: 1 / span 4;
-            grid-row: 1 / span 3;
-        }
-
-        .img-2 {
-            grid-column: 1 / span 4;
-            grid-row: 4 / span 6;
-        }
-
-        .img-3 {
-            grid-column: 1 / span 4;
-            grid-row: 10 / span 3;
-        }
     }
 </style>
